b7332272b6b42a2626fb01b633a26acbf26ab058
Joshua Birks <joshuabirks30@gmail.com>
2024-02-25 04:01:04 +0000

    Implementing the getAccessToken function by loading the verifier from local storage and using the verifier and the code from the callback to perform a POST to the Spotify token API and generate an access token using these values to verify the request

--- spotify-profile-demo/src/script.ts
+++ spotify-profile-demo/src/script.ts
@@ -47,7 +47,23 @@ async function generateCodeChallenge(codeVerifier: string) {
 }
 
 async function getAccessToken(clientId: string, code: string) {
-    // TODO: Get access token for code
+    const verifier = localStorage.getItem("verifier");
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", "http://localhost:5174/callback");
+    params.append("code_verifier", verifier!);
+
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: params
+    });
+
+    const { access_token } = await result.json();
+    return access_token;
 }
 
 async function fetchProfile(token: string): Promise<any> {
