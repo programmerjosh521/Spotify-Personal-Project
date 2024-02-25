ffcc407ac226caac48f737b9a6f767f7b523c500
Joshua Birks <joshuabirks30@gmail.com>
2024-02-25 03:40:18 +0000

    Basic framework for the app including calling the Web API

---
+++ spotify-profile-demo/src/script.ts
@@ -0,0 +1,26 @@
+const clientId = "915f22791a204275a6a2f26b9bca6ca4";
+const code = undefined;
+
+if (!code) {
+    redirectToAuthCodeFlow(clientId);
+} else {
+    const accessToken = await getAccessToken(clientId, code);
+    const profile = await fetchProfile(accessToken);
+    populateUI(profile);
+}
+
+async function redirectToAuthCodeFlow(clientId: string) {
+    // TODO: Redirect to Spotify authorization page
+}
+
+async function getAccessToken(clientId: string, code: string) {
+    // TODO: Get access token for code
+}
+
+async function fetchProfile(token: string): Promise<any> {
+    // TODO: Call Web API
+}
+
+function populateUI(profile: any) {
+    // TODO: Update UI with profile data
+}
